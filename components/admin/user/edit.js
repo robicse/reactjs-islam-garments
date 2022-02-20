5d5408ce014ace728713db2e362c492bf25759db
robeul <robeul.starit@gmail.com>
2022-02-20 21:33:53 +0600

    'userEdit'

--- components/admin/user/edit.js
+++ components/admin/user/edit.js
@@ -14,6 +14,7 @@ import { baseUrl } from "../../../const/api";
 import { useAsyncEffect } from "use-async-effect";
 import axios from "axios";
 import AllApplicationErrorNotification from "../../utils/errorNotification";
+import { values } from "mobx";
 
 const styles = {
   cardCategoryWhite: {
@@ -43,6 +44,7 @@ function Edit({ token, modal, editData, endpoint, mutate }) {
   const [roles, setRoles] = React.useState([]);
   const [warehouse, setWarehouse] = React.useState([]);
   const [store, setStore] = React.useState([]);
+  // const [type, setType] = React.useState(editData.user_for);
   let ware = `${baseUrl}/warehouse_list`;
   let stor = `${baseUrl}/store_list`;
   let rol = `${baseUrl}/roles`;
@@ -67,12 +69,23 @@ function Edit({ token, modal, editData, endpoint, mutate }) {
           setWarehouse(responseOneB.data.data);
           setStore(responseTwoU.data.data);
           setRoles(responseTwoR.data.data);
+
+          
         })
       )
       .catch((errors) => {
         console.error(errors);
       });
   }, []);
+
+
+  
+  // React.useEffect(()=>{
+  //   console.log('type',type);
+  //   setType(type)
+  // },[setType])
+
+  // console.log('user for', type);
 
   return (
     <div>
@@ -85,6 +98,7 @@ function Edit({ token, modal, editData, endpoint, mutate }) {
                   name: editData.name,
                   phone: editData.phone,
                   email: editData.email,
+                  user_for: editData.user_for,
                   role: editData.role,
                   password: "",
                   confirm_password: "",
@@ -93,6 +107,7 @@ function Edit({ token, modal, editData, endpoint, mutate }) {
                   status: editData.status,
                 }}
                 validate={(values) => {
+                  console.log('user_for',values.user_for)
                   const errors = {};
                   if (!values.phone) {
                     errors.phone = "Required";
@@ -113,7 +128,7 @@ function Edit({ token, modal, editData, endpoint, mutate }) {
                 onSubmit={(values, { setSubmitting }) => {
 
 
-                  if (!values.warehouse && !values.store) {
+                  if (editData.role !== "Super Admin" && !values.warehouse && !values.store) {
                     setSubmitting(false);
                     return cogoToast.warn('Please Select Warehouse or Store',{position: 'top-right', bar:{size: '10px'}});
                   }
@@ -128,6 +143,7 @@ function Edit({ token, modal, editData, endpoint, mutate }) {
                         name: values.name,
                         phone: values.phone,
                         email: values.email,
+                        user_for: values.user_for,
                         status: values.status,
                         password: values.password,
                         confirm_password: values.confirm_password,
@@ -207,6 +223,9 @@ function Edit({ token, modal, editData, endpoint, mutate }) {
                               variant="outlined"
                               helperText="Please select roles"
                               margin="normal"
+                              disabled={
+                                editData.role === "Super Admin" ? true : false
+                              }
                             >
                               {roles.map((r) => (
                                 <MenuItem value={r.name} key={r.id}>
@@ -216,48 +235,145 @@ function Edit({ token, modal, editData, endpoint, mutate }) {
                             </Field>
                           </GridItem>
 
-                          <GridItem xs={12} sm={12} md={6}>
-                            <Field
-                              component={TextField}
-                              type="text"
-                              name="warehouse"
-                              label="Warehouse"
-                              select
-                              fullWidth
-                              disabled={
-                                editData.role === "admin" ? true : false
-                              }
-                              variant="outlined"
-                              helperText="Please select warehouse"
-                              margin="normal"
-                            >
-                              {warehouse.map((w) => (
-                                <MenuItem value={w.id}>{w.name}</MenuItem>
-                              ))}
-                            </Field>
-                          </GridItem>
 
-                          <GridItem xs={12} sm={12} md={6}>
-                            <Field
-                              component={TextField}
-                              type="text"
-                              name="store"
-                              label="Store"
-                              select
-                              fullWidth
-                              variant="outlined"
-                              helperText="Please select Store"
-                              disabled={
-                                editData.role === "admin" ? true : false
-                              }
-                              margin="normal"
-                            >
-                              <MenuItem value={null}>Unselect</MenuItem>
-                              {store.map((s) => (
-                                <MenuItem value={s.id}>{s.store_name}</MenuItem>
-                              ))}
-                            </Field>
-                          </GridItem>
+                          {editData.role !== "Super Admin" ? (
+                            <>
+                              <GridItem xs={12} sm={12} md={6}>
+                                <Field
+                                  component={TextField}
+                                  type="text"
+                                  name="user_for"
+                                  // value={type}
+                                  label="User For"
+                                  select
+                                  fullWidth
+                                  variant="outlined"
+                                  helperText="Please select Store"
+                                  margin="normal"
+                                  // onChange={(e) => setType(e.target.value)}
+                                  helperText="Please select one"
+                                >
+                                  <MenuItem value="Warehouse">Warehouse</MenuItem>
+                                  <MenuItem value="Store">Store</MenuItem>
+                                </Field>
+                              </GridItem>
+                              {values.user_for == "Warehouse" ? (
+                                <>
+                                  <GridItem xs={12} sm={12} md={6}>
+                                    <Field
+                                      component={TextField}
+                                      type="text"
+                                      name="warehouse"
+                                      label="Warehouse"
+                                      select
+                                      fullWidth
+                                      variant="outlined"
+                                      helperText="Please select warehouse"
+                                      margin="normal"
+                                    >
+                                      {warehouse.map((w) => (
+                                        <MenuItem value={w.id}>{w.name}</MenuItem>
+                                      ))}
+                                    </Field>
+                                  </GridItem>
+                                </>
+                              ) : (
+                                <>
+                                  <GridItem xs={12} sm={12} md={6}>
+                                    <Field
+                                      component={TextField}
+                                      type="text"
+                                      name="store"
+                                      label="Store"
+                                      select
+                                      fullWidth
+                                      variant="outlined"
+                                      helperText="Please select Store"
+                                      margin="normal"
+                                    >
+                                      <MenuItem value={null}>Unselect</MenuItem>
+                                      {store.map((s) => (
+                                        <MenuItem value={s.id}>{s.store_name}</MenuItem>
+                                      ))}
+                                    </Field>
+                                  </GridItem>
+                                </>
+                              )}
+                            </>
+                          ) : (
+                            <>
+                              
+                            </>
+                          )}
+
+
+                          
+                            {/* <GridItem xs={12} sm={12} md={6}>
+                              <Field
+                                component={TextField}
+                                type="text"
+                                name="user_for"
+                                // value="Warehouse"
+                                label="User For"
+                                select
+                                fullWidth
+                                variant="outlined"
+                                helperText="Please select Store"
+                                margin="normal"
+                                onChange={(e) => setType(e.target.value)}
+                                helperText="Please select one"
+                              >
+                                <MenuItem value="Warehouse">Warehouse</MenuItem>
+                                <MenuItem value="Store">Store</MenuItem>
+                              </Field>
+                            </GridItem>
+
+                            {type == "Warehouse" ? (
+                            <GridItem xs={12} sm={12} md={6}>
+                              <Field
+                                component={TextField}
+                                type="text"
+                                name="warehouse"
+                                label="Warehouse"
+                                select
+                                fullWidth
+                                disabled={
+                                  editData.role === "Super Admin" ? true : false
+                                }
+                                variant="outlined"
+                                helperText="Please select warehouse"
+                                margin="normal"
+                              >
+                                {warehouse.map((w) => (
+                                  <MenuItem value={w.id}>{w.name}</MenuItem>
+                                ))}
+                              </Field>
+                            </GridItem>
+                            ): (
+                            <GridItem xs={12} sm={12} md={6}>
+                              <Field
+                                component={TextField}
+                                type="text"
+                                name="store"
+                                label="Store"
+                                select
+                                fullWidth
+                                variant="outlined"
+                                helperText="Please select Store"
+                                disabled={
+                                  editData.role === "Super Admin" ? true : false
+                                }
+                                margin="normal"
+                              >
+                                <MenuItem value={null}>Unselect</MenuItem>
+                                {store.map((s) => (
+                                  <MenuItem value={s.id}>{s.store_name}</MenuItem>
+                                ))}
+                              </Field>
+                            </GridItem>
+                            )} */}
+
+                          
 
                           <GridItem xs={12} sm={12} md={12}>
                             <Field

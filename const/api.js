0f296eaa8c6736db486633e805a09690ac255a19
robeul <robeul.starit@gmail.com>
2022-03-22 06:54:55 +0600

    'hideStorToStore'

--- const/api.js
+++ const/api.js
@@ -7,7 +7,7 @@ const host = 'islam-garments-backend.starit-ltd.xyz'
 const host2 = 'demo-islam-garments-backend.starit-ltd.xyz'
 
 const api = '/api';
-export const baseUrl = `${protocol + host2 + api}`;
+export const baseUrl = `${protocol + host + api}`;
 
 
-export const webUrl = `${protocol + host2}`;
+export const webUrl = `${protocol + host}`;
